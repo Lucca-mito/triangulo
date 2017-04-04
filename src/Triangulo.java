cdcc85b26d55da0fc090f485c277f86eeab917ee
Lucca de Mello <lucca.mello@ad.ort.org.br>
2017-04-04 16:15:11 -0300

    Lucca: inicializa os Scanners com ajuda de ninguem

--- src/Triangulo.java
+++ src/Triangulo.java
@@ -1,9 +1,17 @@
-
+import java.util.Scanner;
 public class Triangulo {
 
+	public boolean existeTriangulo(int a, int b, int c) {
+		return (a + b > c) && (a + c > b) && (b + c > a);
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		int a = input.nextInt(),
+			b = input.nextInt(),
+			c = input.nextInt(),
+			d = input.nextInt();
+		return ()
 	}
 
 }

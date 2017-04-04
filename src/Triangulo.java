872fca5b89108fd6c991028151e67921dfee100d
Lucca de Mello <lucca.mello@ad.ort.org.br>
2017-04-04 16:45:18 -0300

    Thiago: otimiza o codigo e faz rodar o codigo

--- src/Triangulo.java
+++ src/Triangulo.java
@@ -11,39 +11,23 @@ public class Triangulo {
 			b = input.nextInt(),
 			c = input.nextInt(),
 			d = input.nextInt();
-		for(int i=0; i < 4; i++){
-		switch(i) {
-		case 0:
+		
 			if (existeTriangulo(a,b,c)){
 				 System.out.println("S");
+				 
+			}
+			else if (existeTriangulo(a,b,d)){
+				 System.out.println("S");
 			 }
-			 else{
-				 System.out.println("N");
+			else if (existeTriangulo(b,d,c)){
+				 System.out.println("S");
 			 }
-		case 1:
-			if (existeTriangulo(a,b,d)){
+			else if (existeTriangulo(a,d,c)){
 				 System.out.println("S");
 			 }
 			 else{
 				 System.out.println("N");
-			 }
-		case 2:
-			if (existeTriangulo(b,d,c)){
-				 System.out.println("S");
-			 }
-			 else{
-				 System.out.println("N");
-			 }
-		case 3:
-			if (existeTriangulo(a,d,c)){
-				 System.out.println("S");
-			 }
-			 else{
-				 System.out.println("N");
-			 }
-		
+				 }
 		}
 	}
-	}
 
-}
